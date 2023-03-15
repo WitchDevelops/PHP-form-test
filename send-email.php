f85c25c2508e66189292f779c8179feb4d5a5a20
W1ckedW1tch <112077394+WitchDevelops@users.noreply.github.com>
2023-03-15 11:38:38 +0100

    try new php code

--- send-email.php
+++ send-email.php
@@ -1,43 +1,35 @@
 <?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  require 'PHPMailer-master/src/Exception.php';
+  require 'PHPMailer-master/src/PHPMailer.php';
+  require 'PHPMailer-master/src/SMTP.php';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
 
-require "vendor/autoload.php";
-
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+  $mail->SMTPDebug  = 0;  
+  $mail->SMTPAuth   = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port       = 587;
+  $mail->Host       = "smtp.gmail.com";
+  $mail->Username   = "domi@mokosh.dev";
+  $mail->Password   = "M0k0shd0td3v";
 
 $email = $_POST["email"];
 $message = $_POST["message"];
 
-$mail = new PHPMailer(true); //create a mailer class
+  $mail->IsHTML(true);
+  $mail->AddAddress("groaning.witch@gmail.com");
+  $mail->SetFrom($email);
+$mail->Subject = "New contact form submission";
+  $content = $message;
 
-try {
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-
-// access my smtp server 
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-$mail->Port = 587;
-
-$mail->Username = "domi@mokosh.dev";
-$mail->Password = "M0k0shd0td3v";
-
-// pass in the values from the contact form
-$mail->setFrom($email);
-$mail->addAddress("groaning.witch@gmail.com"); //send to this address
-$mail->isHTML(true); 
-$mail->Body = $message;
-
-$mail->send(); //send it
-
-echo "form submitted successfully";
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+  $mail->MsgHTML($content); 
+  if(!$mail->Send()) {
+    echo "Error while sending Email.";
+    var_dump($mail);
+  } else {
+    echo "Email sent successfully";
+  }
+?>
